bd3d0bb65aff3bda2e4da0cc150eb7b73244b1ad
rintaro1029 <tomitaka1029@gmail.com>
2024-01-25 16:22:44 +0900

    add cancel function

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { json, redirect } from "@remix-run/node";
 
@@ -9,6 +9,7 @@ import { getContact, updateContact } from "../data";
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
+
   if (!contact) {
     throw new Response("Not Found", { status: 404 });
   }
@@ -25,6 +26,7 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
 
 export default function EditContact() {
   const { contact } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
 
   return (
     <Form key={contact.id} id="contact-form" method="post">
@@ -53,7 +55,9 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button onClick={() => navigate(-1)} type="button">
+          Cancel
+        </button>
       </p>
     </Form>
   );
